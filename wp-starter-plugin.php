d1ca7f909ee5d1cfbec1130c6473e5ffaa800e7d
Dominik Tränklein <dominik@bleech.de>
2016-10-31 21:01:28 +0100

    build(setup): initial commit

---
+++ wp-starter-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Wp Starter Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-starter-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Starter_Plugin
+ */
