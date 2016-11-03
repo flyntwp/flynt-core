021dbe72a61249c0e3a545ef8c2aae9a3b5c5b31
Dominik Tränklein <dominik@bleech.de>
2016-11-03 13:06:59 +0100

    feat(helpers): add extractNestedDataFromArray helper function

---
+++ tests/test-helpers.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Class ConstructionPlanTest
+ *
+ * @package Wp_Starter_Plugin
+ */
+
+/**
+ * Construction plan test case.
+ */
+
+use PHPUnit\Framework\TestCase;
+use function WPStarter\Helpers\extractNestedDataFromArray;
+
+class HelpersTest extends TestCase {
+  function testExtractNestedDataFromArray() {
+    $bar = 'baz';
+    $foo = [
+      'bar' => $bar
+    ];
+    $arr = [
+      'foo' => $foo
+    ];
+    $value = extractNestedDataFromArray([$arr, 'foo']);
+    $this->assertEquals($value, $foo);
+    $value = extractNestedDataFromArray([$arr, 'foo', 'bar']);
+    $this->assertEquals($value, $bar);
+  }
+}
--- wp-starter-plugin.php
+++ wp-starter-plugin.php
@@ -12,4 +12,5 @@
  * @package         Wp_Starter_Plugin
  */
 
+require_once __DIR__ . '/lib/WPStarter/Helpers.php';
 require_once __DIR__ . '/lib/WPStarter/ConstructionPlan.php';
