966cd7762857e976a70f268a70ba8fec742e0d8c
Doğa Gürdal <doga@bleech.de>
2016-11-14 13:12:55 +0100

    feat(helpers): extractData - added ability to use array further down the param line

--- lib/WPStarter/Helpers.php
+++ lib/WPStarter/Helpers.php
@@ -4,7 +4,8 @@ namespace WPStarter;
 
 class Helpers {
   public static function extractNestedDataFromArray($args = []) {
-    if (count($args) <= 1) return '';
+    if (count($args) == 0) return '';
+    if (count($args) == 1) return $args[0];
     $key = array_pop($args);
     if (count($args) > 1) {
       $data = self::extractNestedDataFromArray($args);
@@ -14,10 +15,11 @@ class Helpers {
     $output = '';
     if ($key === '*') {
       $output = $data;
+    } elseif (is_array($key)) {
+      $output = $key;
     } elseif (is_array($data) && array_key_exists($key, $data)) {
       $output = $data[$key];
     }
-
     return $output;
   }
 }
